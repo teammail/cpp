35aa7c5f54b7b3d29c0e09601ce88178f5582803
Denis Stepulenok <super.denis@gmail.com>
2015-01-27 20:06:31 +0300

    Save changes

--- lesson_02/HomeWork_1/main.cpp
+++ lesson_02/HomeWork_1/main.cpp
@@ -23,16 +23,25 @@ typedef Point Vector;
 // Сумма
 Point sum(Vector A, Vector B){
   // TODO: Реализовать
+  Point R;
+  R.x = A.x + B.x;
+  R.y = A.y + B.y;
+  return R;
 }
 
 // Разность
 Point sub(Vector A, Vector B){
   // TODO: Реализовать
+  Point R;
+  R.x = A.x - B.x;
+  R.y = A.y - B.y;
+  return R;
 }
 
 // Скалярное произведение векторов
-Point dotProduct(Vector A, Vector B){
+double dotProduct(Vector A, Vector B){
   // TODO: Реализовать
+  return A.x * B.x + A.y * B.y;
 }
 
 int main()
@@ -40,5 +49,9 @@ int main()
     Point a = {1.2, 1.3},
           b = {10.2, 3.3};
     cout << dist(a,b) << endl;
+
+    // Тестирование
+    Point c = sub(a,b);
+
     return 0;
 }

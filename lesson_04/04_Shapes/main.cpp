022fd4a9d2ccba73a534c9695cff07f18802ee09
Denis Stepulenok <super.denis@gmail.com>
2015-01-29 21:07:54 +0300

    Save changes

--- lesson_04/04_Shapes/main.cpp
+++ lesson_04/04_Shapes/main.cpp
@@ -6,19 +6,48 @@ using namespace std;
 class Shape {
   // Имя фигуры (строка)
   char *name; // Обязательно private
+protected:
+  // Конструктор для вызова из наследников
+  Shape(char *name = NULL) : name(name) {
+  }
+  // Каждая фигура показывает своё имя
+  void show(){
+    if(name != NULL){
+      cout << name << " ";
+    }
+  }
 };
 
 // Наследники Shape:
-class Point
+class Point : protected Shape {
+  double x, y;
+public:
   // Конструктор
+  Point(char *name, double x, double y) :
+      Shape(name), x(x), y(y) {
+  }
+};
 
-class Line
-// Внутри 2 точки
+class Line : protected Shape {
+  // Внутри 2 точки
+  Point a, b;
+public:
   // Конструктор
+  Line(char *name, Point a, Point b) :
+    Shape(name), a(a), b(b){
+  }
+};
 
-class Circle
-// Одна точка и радиус
+class Circle : protected Shape {
+  // Одна точка и радиус
+  Point center;
+  double radius;
   // Конструктор
+public:
+  Circle(char *name, Point center, double radius) :
+    Shape(name), center(center), radius(radius){
+  }
+};
 
 // Везде метод show() -> показывает свойства фигуры
 
@@ -27,10 +56,10 @@ int main() {
   Line line1("AB", A, B);
   Circle c1("CircleA", A, 5.0),
     c2("CircleB", B, 7.2);
-  A.show(); B.show();
+  /*A.show(); B.show();
   line1.show();
   c1.show();
-  c2.show();
+  c2.show(); */
 
   return 0;
 }
